de39cff715747b885b0a385ae103320029867dad
Helene Andre <helene.andre.06@gmail.com>
2020-04-06 18:30:38 +1000

    Animations quote.

--- src/components/quote.vue
+++ src/components/quote.vue
@@ -1,14 +1,12 @@
 <template lang="pug">
-#quote(@mouseover="show = true" @mouseleave="show = false")
-  transition(name="slide")
-    .quote {{ quote }}
-  transition(name="fade")
-    .quote-author(v-if="show") {{ info }}
-      span.author {{ author }}
-      span
-        i#like-quote.icon.icon-heart-o.icon-link
-        a(href='https://twitter.com/intent/tweet?url=http%3A%2F%2Fhelene-andre.github.io/Momentum%2F&text=Awesome+web+app!&hashtags=coolwebsite', target='_blank')
-          i#twitter.icon.icon-twitter.icon-link
+.quote
+  .quote__sentence {{ quote }}
+  .quote__author {{ info }}
+    span.author {{ author }}
+    span
+      i.icon(@click="updatelikedQuoteLocal()" :class="{ 'icon-heart-o': !likedQuote, 'icon-heart': likedQuote }")
+      a(href='https://twitter.com/intent/tweet?url=http%3A%2F%2Fhelene-andre.github.io/Momentum%2F&text=Awesome+web+app!&hashtags=coolwebapp', target='_blank')
+        i.icon.icon-twitter
 </template>
 
 <script>
@@ -17,47 +15,93 @@ export default {
     quote: '',
     author: '',
     info: null,
-    show: false
+    likedQuote: 0
   }),
   mounted () {
     const axios = require('axios')
     axios
       .get('https://quotes.rest/qod')
       .then(response => (this.getQuoteOfTheDay(response)))
+      .catch(() => {this.getQuoteOfTheDay()})
+
+    this.likedQuote = localStorage.likedQuote === '0'
   },
   methods: {
+    updatelikedQuoteLocal () {
+      this.likedQuote = !this.likedQuote
+      localStorage.likedQuote = this.likedQuote
+    },
     getQuoteOfTheDay (response) {
-        if (response.data.contents.quotes[0].quote) {
-          this.quote = `“${response.data.contents.quotes[0].quote}”`
-          this.author = response.data.contents.quotes[0].author
-        }
-        else {
-          this.quote = '“Those who dare to fail miserably can achieve greatly.”'
-          this.author = 'John F. Kennedy'
-        }
+      if (response) {
+        this.quote = `“${response.data.contents.quotes[0].quote}”`
+        this.author = response.data.contents.quotes[0].author
+      }
+      else {
+        this.quote = '“Those who dare to fail miserably can achieve greatly.”'
+        this.author = 'John F. Kennedy'
+      }
     }
   }
 }
 </script>
 
 <style lang="scss">
-#quote {
+.quote {
   position: absolute;
-  bottom: 0;
+  bottom: 12px;
   left: 50%;
   transform: translateX(-50%);
-  width: 70%;
+  width: 65%;
   text-align: center;
-  font-size: 15px;
+  font-size: 16px;
+  padding: 10px;
+  text-align: center;
+  &__author {
+    position: relative;
+    width: 100%;
+    opacity: 0;
+    transition: all 0.3s ease-in-out;
+    & .author {
+      opacity: 0.7;
+      font-size: 13px;
+    }
+    & span, & i {padding: 0 2px 0 2px;}
+    & .icon-heart-o {
+      opacity: 0.7;
+      &:hover {opacity: 1;}
+    }
+  }
+  &__sentence {
+    position: absolute;
+    width: 100%;
+    transform: translateY(-10px);
+    transition: all 0.3s ease-in-out;
+  }
+  &:hover {
+    & .quote__author {
+      opacity: 1;
+      transform: translateY(3px);
+    }
+    & .quote__sentence {transform: translateY(-100%);}
+  }
 }
 
-.fade-enter-active, .fade-leave-active {
-  transition: all 0.3s ease-in-out;
+//=============================== media queries =====================================//
+@media screen and (max-width: 1050px) {
+  .quote{width: 50%;}
 }
 
-.fade-enter, .fade-leave-to {
-  opacity: 0;
-  transform: translateY(-10px);
+@media screen and (max-width: 800px) {
+  .quote{width: 40%;}
 }
+
+@media screen and (max-width: 700px) {
+  .quote{width: 30%;}
+}
+
+@media screen and (max-width: 500px) {
+  .quote{width: 20%;}
+}
+//===================================================================================//
 </style>
 
